f9a3918f9362c9ad438887005f4d63c11fd71f9b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-24 10:00:18 -0300

    criado as configurações strings de conexao com banco com api

--- 3.fastapi/core/configs.py
+++ 3.fastapi/core/configs.py
@@ -1,1 +1,8 @@
- 
+from typing import List
+from pydantic import BaseSettings, AnyHttpUrl
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    # CONFIGURAÇÕES GERAIS USADAS NA APLICAÇÃO
+    API_V1_STR: str = '/api/p1'
+    DB_URL: str = 'postgresql+asyncpg://user:password@localhost:5432/facaldade'
