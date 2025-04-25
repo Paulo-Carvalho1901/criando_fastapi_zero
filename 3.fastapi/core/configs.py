eabd06d237109fd9b021e1213171e28fca8d59f8
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-25 15:55:12 -0300

    alteramdo API_V1_STR para api;v1

--- 3.fastapi/core/configs.py
+++ 3.fastapi/core/configs.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # CONFIGURAÇÕES GERAIS USADAS NA APLICAÇÃO
-    API_V1_STR: str = '/api/p1'
+    API_V1_STR: str = '/api/v1'
     DB_URL: str = 'postgresql+asyncpg://postgres:D@vi0406@localhost:5432/faculdade'
     DBBaseModel = declarative_base()
 
