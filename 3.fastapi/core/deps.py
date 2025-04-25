96e7b52414f6d593ee7c767eee55e53940ff7c55
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-25 16:06:02 -0300

    alterando a função generator

--- 3.fastapi/core/deps.py
+++ 3.fastapi/core/deps.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import Session
 
 
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_session() -> AsyncGenerator:
     session: AsyncSession = Session()
 
     try:
