a71ebc8ab53002775e750f32a2c0cc8de3d13d66
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-28 17:21:12 -0300

    criado: post cursoo fastapi

--- 3.fastapi/api/v1/endpoints/curso.py
+++ 3.fastapi/api/v1/endpoints/curso.py
@@ -17,3 +17,14 @@ from models.curso_models import CursoModel
 from schemas.curso_schema import CursoSchema
 from core.deps import get_session
 
+router = APIRouter()
+
+# POST curso
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
+async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(titulo=curso.titulo, aulas=curso.aulas, horas=curso.horas)
+
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
