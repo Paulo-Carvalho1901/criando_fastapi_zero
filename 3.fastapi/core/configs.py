0045af14cc4d2d65f3f31a6409d4974411641f73
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-28 16:24:30 -0300

    banco de dado conectado com sucesso

--- 3.fastapi/core/configs.py
+++ 3.fastapi/core/configs.py
@@ -1,12 +1,11 @@
 from pydantic.v1 import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
-from typing import ClassVar
 
 
 class Settings(BaseSettings):
     # CONFIGURAÇÕES GERAIS USADAS NA APLICAÇÃO
     API_V1_STR: str = '/api/v1' # Versão da API
-    DB_URL: str = "postgresql+asyncpg://postgres:D@vi0406d@127.0.0.1:5432/faculdade"
+    DB_URL: str = "postgresql+asyncpg://postgres:D%40vi0406@127.0.0.1:5432/faculdade"
     DBBaseModel = declarative_base()
 
     class Config:
