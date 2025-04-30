917a1cb56d763e3ffe8fe9a598c78709383b140f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-30 01:26:45 -0300

    Criado: ger_curso que retorna pelo id do curso

--- 3.fastapi/api/v1/endpoints/curso.py
+++ 3.fastapi/api/v1/endpoints/curso.py
@@ -19,6 +19,7 @@ from core.deps import get_session
 
 router = APIRouter()
 
+
 # POST curso
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
 async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)):
@@ -29,6 +30,7 @@ async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)
 
     return novo_curso
 
+
 # GET cursos
 @router.get('/', response_model=List[CursoSchema])
 async def get_cursos(db: AsyncSession = Depends(get_session)):
@@ -38,3 +40,17 @@ async def get_cursos(db: AsyncSession = Depends(get_session)):
         cursos: List[CursoModel] = result.scalars().all()
 
         return cursos
+
+
+# GET curso]
+@router.get('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso = result.scalar_one_or_none()
+        
+        if curso:
+            return curso
+        else:
+            raise HTTPException(detail='Curso não encontrado', status_code=status.HTTP_404_NOT_FOUND)
