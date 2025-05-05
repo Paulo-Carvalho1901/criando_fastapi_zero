41271c160680d018313d94446f556ef2124d566b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-05 14:30:37 -0300

    criando a string de conexão com o banco postgres

---
+++ 4.fastapi/core/configs.py
@@ -0,0 +1,11 @@
+from pydantic.v1 import BaseSettings
+
+class Settings(BaseSettings):
+    API_V1_STR: str = 'api/v1'
+    DB_URL: str = "postgresql+asyncpg://postgres:D%40vi0406@127.0.0.1:5432/faculdade"
+
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings = Settings()
