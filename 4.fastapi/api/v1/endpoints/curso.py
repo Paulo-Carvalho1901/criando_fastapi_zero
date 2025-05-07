9d29f88ef14667bf5c185e8456d59c2555bcdcd9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-07 03:04:40 -0300

    criado o endpoint do post curso

--- 4.fastapi/api/v1/endpoints/curso.py
+++ 4.fastapi/api/v1/endpoints/curso.py
@@ -12,3 +12,25 @@ from sqlmodel import select
 from models.curso_model import CursoModel
 from core.deps import get_session
 
+# Bypass worning SQLModel select
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+SelectOfScalar.inherit_cache = True # type ignore
+Select.inherit_cache = True # # type ignore
+# Fim Bypass
+
+router = APIRouter()
+
+# POST CURSO
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoModel)
+async def post_curso(curso: CursoModel, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(
+        titulo=curso.titulo,
+        aulas=curso.aulas,
+        horas=curso.horas
+    )
+
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
