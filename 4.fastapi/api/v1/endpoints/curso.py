8d6ae3916d2e5d1eed36f6ae468aa87265cd5db6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-07 02:53:57 -0300

    criado: estrutura base da api fastapi com os imports necessarios.

---
+++ 4.fastapi/api/v1/endpoints/curso.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from fastapi import APIRouter
+from fastapi import status
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Response
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
+
+from models.curso_model import CursoModel
+from core.deps import get_session
+
