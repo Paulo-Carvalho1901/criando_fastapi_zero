107ac580d44910cf7ccc3b7bc9ec32245877bde7
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-08 02:33:36 -0300

    criado o put e delete dos endpoints do crud da api

--- 4.fastapi/api/v1/endpoints/curso.py
+++ 4.fastapi/api/v1/endpoints/curso.py
@@ -61,4 +61,39 @@ async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
 
 
 # PUT CURSO
+@router.put('/{curso_id}', status_code=status.HTTP_202_ACCEPTED, response_model=CursoModel)
+async def put_curso(curso_id: int, curso: CursoModel, db: AsyncSession = Depends(get_session)):
+     async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_up: CursoModel = result.scalar_one_or_none()
 
+        if curso_up:
+            curso_up.titulo = curso.titulo
+            curso_up.aulas = curso.aulas
+            curso_up.horas = curso.horas
+            
+            await session.commit()
+
+            return curso_up
+        else:
+            raise HTTPException(detail="curso não encontrado", status_code=status.HTTP_404_NOT_FOUND)
+
+
+
+# DELETE CURSO
+@router.delete('/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del: CursoModel = result.scalar_one_or_none()
+
+        if curso_del:
+            await session.delete(curso_del)
+            await session.commit()
+
+            
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail="curso não encontrado", status_code=status.HTTP_404_NOT_FOUND)
