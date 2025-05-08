70ac811ac7d456d2d53123ee8d93c92c621e1ad2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-08 15:26:51 -0300

    organizando os importes da fastapi

--- 4.fastapi/api/v1/endpoints/curso.py
+++ 4.fastapi/api/v1/endpoints/curso.py
@@ -1,10 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter
-from fastapi import status
-from fastapi import Depends
-from fastapi import HTTPException
-from fastapi import Response
+from fastapi import APIRouter, status, Depends, HTTPException, Response
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
