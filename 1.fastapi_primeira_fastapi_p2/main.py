d5f7dce84eedba0f40fba2124efacd97086e49b9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-09 01:49:11 -0300

    criado entrada para execução do main

--- 1.fastapi_primeira_fastapi_p2/main.py
+++ 1.fastapi_primeira_fastapi_p2/main.py
@@ -3,7 +3,13 @@ from fastapi import FastAPI
 # Intanciancio um objeto
 app = FastAPI()
 
-@app.get('/')
-async def root():
+@app.get('/mensagem') # endpoints da nossa api
+async def mensagem():
     return {"msg": "criado minha primeira API com FasTAPI"}
 
+
+# CRIANDO UMA ENTRADA PARA EXECUÇÃO
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level='info', reload=True)
